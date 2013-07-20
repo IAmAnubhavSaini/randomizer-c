8bde97a8d7a867edacabbac1726b5038509f94d4
IAmAnubhavSaini <iamanubhavsaini@gmail.com>
2013-07-20 17:42:37 +0530

    random integer generator code checked.

--- src/random_integers.c
+++ src/random_integers.c
@@ -2,15 +2,22 @@
 #include <stdlib.h>
 
 void initiate_randomness(void){
-  srand(time(NULL));
 #ifndef INITIATED_RANDOMNESS
+  srand(time(NULL));
 #define INITIATED_RANDOMNESS
 #endif
 }
 int generate_random_number(void){
-#ifndef INITIATED_RANDOMNESS
   initiate_randomness();
-#endif
   int r = rand();
+  return r;
 }
-
+int generate_limited_random_number(int limit){
+  int divisor = RAND_MAX/(limit+1);
+  int number = 0;
+  initiate_randomness();
+  do{
+    number = rand()/divisor;
+  }while(number > limit);
+  return number;
+}
